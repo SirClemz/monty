6513afb077e5a075cad8424a5a816677e799caf3
frankytech <emenikefc16082@gmail.com>
2022-12-16 03:26:12 -0800

    collab

---
+++ rotl.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * f_rot1- function that rotates the stack to the top
+ * @head : hezd of the stack
+ * @counter: line counter
+ *
+ * retrurn : nothing
+ */
+
+void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
+{
+	stack_t *tmp = *head, *aux;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+	
+		return;
+	}
+	aux = (*head)->next;
+	aux->prev = NULL;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = *head;
+	(*head)->next = NULL;
+	(*head)->prev = tmp;
+	(*head) = aux;
+
+}
